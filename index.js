f703234aa6a560c923acf235c202b1d9540057a7
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 01:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 const returnFirstTwoDrivers = function(drivers) {return drivers.slice(0,2)};
 const returnLastTwoDrivers = function(drivers) {return drivers.slice(-2,drivers.length)};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function(fare) {int*fare};
+}
